190aeb97c30c40f2be3b1bf6c19acab59b49a2d6
Jonathan Dias <dev2@jaasolucoes.com.br>
2022-07-31 22:28:52 -0300

    Add project files.

---
+++ QRCodeApp/Models/QrCodeGenerator.cs
@@ -0,0 +1,18 @@
+﻿using QRCoder;
+using System.Drawing;
+
+namespace QRCodeApp.Models
+{
+    public static class QrCodeGenerator
+    {
+        public static byte[] GenerateImage(string url)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            BitmapByteQRCode qrCode = new BitmapByteQRCode(qrCodeData);
+            var qrCodeImage = qrCode.GetGraphic(10);
+
+            return qrCodeImage;
+        }
+    }
+}
